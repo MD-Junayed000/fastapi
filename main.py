d4c623a0f7c68ea530c403f2d80b7007f9d885d2
MD-Junayed000 <u2008023@student.cuet.ac.bd>
2026-02-01 15:35:11 +0600

    connecting Fastapi to database

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.util.init_db import create_tables
 
-app=FastAPI()
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    # Intializes the db tables when the application starts up
+    print("Created")
+    create_tables()
+    yield # seperation point
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/health")
 def health_check():
